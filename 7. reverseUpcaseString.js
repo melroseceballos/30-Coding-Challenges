fb872cb7aef6b1ef93f93b6f5f23bb1240c109e3
Scott Draper <scott.draper@generalassemb.ly>
2023-02-08 09:34:48 -0700

    Added all coding challenges

---
+++ 7. reverseUpcaseString.js
@@ -0,0 +1,9 @@
+/*-----------------------------------------------------------------
+Challenge: 07-reverseUpcaseString
+Difficulty: Basic
+Prompt:
+- Write a function called reverseUpcaseString that accepts a single string argument, then returns the string with its characters in reverse order and converts all characters to uppercase.
+Examples:
+reverseUpcaseString("SEI Rocks!"); //=> "!SKCOR IES"
+-----------------------------------------------------------------*/
+// Your solution for 07-reverseUpcaseString here:
