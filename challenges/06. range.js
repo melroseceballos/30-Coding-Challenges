120e9fe43f78de70560324938983221b491bd0c7
Melrose Ceballos <melroseceballos@Melroses-MBP.lan>
2023-02-19 11:31:09 -0800

    Challenge 6 done

--- challenges/06. range.js
+++ challenges/06. range.js
@@ -11,3 +11,17 @@ range(1,1) //=> []
 range(5,2) //=> "First argument must be less than second"
 -----------------------------------------------------------------*/
 // Your solution for 06-range here:
+function range (num1,num2){
+if (num1<=num2){
+    let arr = [];
+    for( let i = num1; i<num2; i++){
+        arr.push(i);
+    } return arr 
+}else{
+    return "First argument must be less than second"
+}
+}
+console.log(range (1,4));
+console.log(range (-2,3));
+console.log(range (1,1));
+console.log(range (5,2));
