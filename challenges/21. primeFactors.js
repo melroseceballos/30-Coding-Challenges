e51b453ef0d33351f31824381ec34838b11ef59b
Melrose Ceballos <melroseceballos@Melroses-MacBook-Pro.local>
2023-03-27 21:01:48 -0700

    Ex 21 done

--- challenges/21. primeFactors.js
+++ challenges/21. primeFactors.js
@@ -16,4 +16,30 @@ primeFactors(105) //=> [3, 5, 7]
 primeFactors(200) //=> [2, 2, 2, 5, 5]
 Hint: Code a nested isPrime(n) helper function that returns true if n is prime, otherwise it returns false
 -----------------------------------------------------------------*/
-// Your solution for 21-primeFactors here:
+// Your solution for 21-primeFactors here: 
+function primeFactors(num){
+    let factors = [];
+    if(num > 1){
+        for(let i = 2; i <= num; i++){
+            while (num % i === 0){
+                factors.push(i);
+                num /= i;
+
+            }
+        }
+        return factors;
+    }else{
+        let empty = []
+        return empty
+    }
+
+}
+
+console.log(primeFactors(2)); // Output: [2]
+console.log(primeFactors(3)); // Output: [3]
+console.log(primeFactors(4)); // Output: [2, 2]
+console.log(primeFactors(18)); // Output: [2, 3, 3]
+console.log(primeFactors(29)); // Output: [29]
+console.log(primeFactors(105)); // Output: [3, 5, 7]
+console.log(primeFactors(200)); // Output: [2, 2, 2, 5, 5]
+console.log(primeFactors(1));
